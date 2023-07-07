913715047f0068a6a52ffc97b4dc8d1d46c7300e
Sebastian <seb.plson@gmail.com>
2023-07-07 15:39:22 +0200

    Add diplomacy defines

--- common/defines/la-defines.lua
+++ common/defines/la-defines.lua
@@ -1,2 +1,8 @@
 -- Military
+NDefines.NMilitary.WARGOAL_MAX_BONUS = 35
 NDefines.NMilitary.BACK_LINE_MORALE_DAMAGE_TAKEN_MODIFIER = 0 -- Artillery fix
+
+-- Diplomacy
+NDefines.NDiplomacy.MIN_RELATIONS_TO_ALLY = -200
+NDefines.NDiplomacy.CHANGE_RIVAL_YEARS = 0
+NDefines.NCountry.PS_REPLACE_RIVAL = 0
